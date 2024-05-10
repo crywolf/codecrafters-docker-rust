d903e5d4b611645543e3827a1c3c4a8a4b9de140
Vojtěch Toman <cry.wolf@centrum.cz>
2024-05-10 13:58:54 +0200

    Filesystem isolation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use anyhow::{Context, Result};
+use std::fs;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let dir = std::env::temp_dir().join("sandbox");
+    fs::create_dir_all(&dir)?;
+
+    let executable = dir.join("executable");
+    fs::copy(command, executable)?;
+
+    std::os::unix::fs::chroot(dir)?;
+    std::env::set_current_dir("/")?;
+    fs::create_dir_all("/dev/null")?;
+
+    let output = std::process::Command::new("./executable")
         .args(command_args)
         .output()
         .with_context(|| {
@@ -15,11 +27,12 @@ fn main() -> Result<()> {
             )
         })?;
 
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    print!("{}", std_out);
+    let std_err = std::str::from_utf8(&output.stderr)?;
+    eprint!("{}", std_err);
+
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{}", std_out);
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        eprint!("{}", std_err);
         std::process::exit(output.status.code().unwrap_or(0));
     } else {
         std::process::exit(output.status.code().unwrap_or(1));
