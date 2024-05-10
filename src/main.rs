06e451a86f458daa7364ee1b6dfe4c732d84c95f
Vojtěch Toman <cry.wolf@centrum.cz>
2024-05-10 12:29:49 +0200

    Wireup stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@ fn main() -> Result<()> {
 
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        eprint!("{}", std_err);
     } else {
         std::process::exit(1);
     }
